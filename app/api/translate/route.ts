f21483387077de3664d786e1282e93c52dff801c
Ezra Chung <ezra.chung97@gmail.com>
2023-08-20 18:05:11 -0700

    Cleaned and basic notes

--- app/api/translate/route.ts
+++ app/api/translate/route.ts
@@ -11,21 +11,11 @@ const translate = new Translate({
     projectId:CREDENTIALS.project_id
 });
 
-//For .env file
-// const {Translate} = require('@google-cloud/translate').v2;
-// // import { NextResponse } from 'next/server';
-// require('dotenv').config();
-
-// const two = process.env.OPENAI_API_KEY;
-// console.log(two, "two")
-
-// const CREDENTIALS = JSON.parse(two || '{}');
-
-// const translate = new Translate({
-//     credentials: CREDENTIALS,
-//     projectId:CREDENTIALS.project_id
-// });
-//End of for .env file
+/**
+ * 
+ * @param request 
+ * @returns JSON of translated data
+ */
 
 async function POST(request: Request) {
     const translate = new Translate({
@@ -50,18 +40,3 @@ async function POST(request: Request) {
   module.exports = {
     POST
 };
-
-// async function translateText() {
-//     const text = 'Hello, world!';
-//     const targetLanguage = 'la'; // French
-  
-//     try {
-//       const [translation] = await translate.translate(text, targetLanguage);
-//     //   console.log(`Original: ${text}`);
-//       console.log(`Translation: ${translation}`);
-//       return translation;
-//     } catch (error) {
-//       console.error('Translation error:', error);
-//       throw new Error('Translation failed') //res.status => 400 error Bad Request
-//     }
-//   }
